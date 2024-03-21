0b065ce1d23bfce8109aee4e46ea5bc13d5f7d5e
Shivani Batish <shivanibatish@btinternet.com>
2024-03-21 15:57:33 +0000

    Add functions to make testCases pass

--- classes/data_transformation.py
+++ classes/data_transformation.py
@@ -5,20 +5,39 @@ db = client['starwars']
 
 
 class MongoTransformation:
-    def __init__(self, starship_data, name_of_pilot):
+    def __init__(self, starship_data, name_of_pilot, client, db):
         self.starship_data = starship_data
         self.name_of_pilot = name_of_pilot
+        self.client = client
+        self.db = db
 
-    def get_obj_id(self, name_of_pilot):
-        ID = db.characters.find_one({"name": name_of_pilot}, {"_id": 1})
+    def check_if_name_in_db(self, input_name):
+        output_name = db.characters.find_one({"name": input_name}, {"name": 1})
+        if output_name:
+            return True
+        else:
+            return False
+
+    def get_id_from_name_in_DB(self, name="Luke Skywalker"):
+        ID = db.characters.find_one({"name": name}, {"_id": 1})
         return ID["_id"]
 
+    def data_body_prep(self, name_of_ship=None, model=None, ids=None):
+        data = {"name": name_of_ship,
+                "model": model,
+                "pilot": []
+                }
+        for pilot_name, pilot_id in ids.items():
+            pilot_data = {"name": pilot_name, "character_id": pilot_id}
+            data["pilot"].append(pilot_data)
+        if len(data["pilot"]) == 0:
+            data["pilot"] = None
 
+        return data
 
-
-
-
-
+    def insert_into_mongodb(self, data):
+        result = db.starships.insert_one(data)
+        print("Inserted document ID:", result.inserted_id)
 
 
 
--- testCases/data_transformation_test.py
+++ testCases/data_transformation_test.py
@@ -1,5 +1,6 @@
 import unittest
 from classes.data_transformation import MongoTransformation
+import bson
 
 
 class MyTestCase(unittest.TestCase):
@@ -13,9 +14,7 @@ class MyTestCase(unittest.TestCase):
 
         self.find_id_in_db = lambda name: None
 
-
-        result = self.check_if_name_in_db("John")
-
+        result = MongoTransformation.check_if_name_in_db(self, input_name="John")
 
         self.assertFalse(result)
 
@@ -23,25 +22,29 @@ class MyTestCase(unittest.TestCase):
 
         self.find_id_in_db = lambda name: "123456"  # Some example ID
 
-        result = self.check_if_name_in_db("Alice")
+        result = MongoTransformation.check_if_name_in_db(self, input_name="Luke Skywalker")
 
         self.assertTrue(result)
 
 
     def test_class_has_a_body_prep_method(self):
-        self.assertTrue(hasattr(MongoTransformation, 'data body prep'))
+        self.assertTrue(hasattr(MongoTransformation, 'data_body_prep'))
 
     def test_body_prep_method_returns_dict(self):
 
-        result = MongoTransformation.data_body_prep()
+        name_of_ship = 'Millennium Falcon'
+        model = "YT-1300 light freighter"
+        ids = {}
+
+        result = MongoTransformation.data_body_prep(self, name_of_ship, model, ids)
 
         # Assert that the return value is a dictionary
         self.assertIsInstance(result, dict)
         self.assertTrue(len(result) == 3)
 
     def test_get_obj_id_from_mongo_with_name(self):
-        result = get_id_from_name_in_DB("Luke Skywalker")
-        self.assertEqual(len(result), 1)
+        result = MongoTransformation.get_id_from_name_in_DB("Luke Skywalker")
+        self.assertEqual(type(result), bson.objectid.ObjectId)
 
 
 
