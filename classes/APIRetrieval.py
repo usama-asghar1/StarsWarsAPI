12619be515d18e67daa3c5a16a09a22aa7779901
Brian Luna <Brian1311@hotmail.co.uk>
2024-03-21 14:14:54 +0000

    change in base URLs

--- classes/APIRetrieval.py
+++ classes/APIRetrieval.py
@@ -1,25 +1,40 @@
 import requests
 
+
+
 class APIRetrieval:
-    def BaseUrl(self,resource_name:str):
-        if resource_name is None:
-            return None
-        else:
-            try:
-                url = f"https://swapi.dev/api/{resource_name}"
-                resource_list = []
-                while url is not None:
-                    response = requests.get(url)
-                    if response.status_code in (200,201):
-                        resource_json = response.json()
-                        url = response.json()['next']
-                        resource_list.extend(resource_json["results"])
-                    else:
-                        print(f'unexpected error:{response.status_code}')
-                        return None
-                return resource_list
-            except requests.exceptions.HTTPError as e:
-                print("HTTP Error: ", e)
+
+    baseUrl = "https://swapi.dev/api/"
+    PeopleUrl = "people/"
+    StarshipUrl = "starships/"
+
+
+    #def findPerson(self,IDnumber):
+    #response = requests.get(baseUrl+people+IDnumber)
+
+
+
+
+
+    # def BaseUrl(self,resource_name:str):
+    #     if resource_name is None:
+    #         return None
+    #     else:
+    #         try:
+    #             url = f"https://swapi.dev/api/{resource_name}"
+    #             resource_list = []
+    #             while url is not None:
+    #                 response = requests.get(url)
+    #                 if response.status_code in (200,201):
+    #                     resource_json = response.json()
+    #                     url = response.json()['next']
+    #                     resource_list.extend(resource_json["results"])
+    #                 else:
+    #                     print(f'unexpected error:{response.status_code}')
+    #                     return None
+    #             return resource_list
+    #         except requests.exceptions.HTTPError as e:
+    #             print("HTTP Error: ", e)
     def get_starships_info(self,resource_url:str):
         if resource_url is None:
             return None
