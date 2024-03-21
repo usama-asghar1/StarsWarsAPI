5913e9a93d2c85e9956575f8f92401b6b8028bc3
Usama <usama.asghar@hotmail.co.uk>
2024-03-21 10:18:10 +0000

    inital commit and failed test

---
+++ data_transformation.py
@@ -0,0 +1,3 @@
+
+class MongoTransformation:
+    pass
---
+++ main.py
@@ -0,0 +1,9 @@
+from unittest import main
+from data_transformation import MongoTransformation
+
+
+
+
+
+# Run unit tests automaticallymain(module='test_module', exit=False)
+main(module='test_module', exit=False)
